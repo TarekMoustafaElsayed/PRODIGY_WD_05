fd00528745b6139874f0ec37a911bda562a38f2b
TarekMoustafaElsayed <150616573+TarekMoustafaElsayed@users.noreply.github.com>
2026-07-05 19:59:44 +0300

    Managed to add the necessary scripts extracting the weather data from weatherAPI website through my api key only thing that is missing is the forecast data

--- script.js
+++ script.js
@@ -5,6 +5,14 @@ const weatherInfoSection = document.querySelector('.weather-info')
 const notFoundSection = document.querySelector('.not-found')
 const searchCitySection = document.querySelector('.search-city')
 
+const countryTxt = document.querySelector('.country-txt')
+const tempTxt = document.querySelector('.temp-txt')
+const conditionTxt = document.querySelector('.condition-txt')
+const humidityValueTxt = document.querySelector('.humidity-value-txt')
+const windValueTxt = document.querySelector('.wind-value-txt')
+const weatherSummaryImg = document.querySelector('.weather-summary-img')
+const currentDateTxt = document.querySelector('.current-date-txt')
+
 searchBtn.addEventListener('click' , () => {
     if(cityInput.value.trim() != '') {
         updateWeatherInfo(cityInput.value)
@@ -29,12 +37,55 @@ async function getFetchData(endPoint, city) {
     return response.json()
 }
 
+function getWeatherIcon(code) {
+    if (code === 1282 || code === 1279 || code === 1276 || code === 1273 || code === 1087) return 'thunderstorm.svg'
+    if (code === 1150 || code === 1153 || code === 1168 || code === 1171 || code === 1072) return 'drizzle.svg'
+    if (code === 1180 || code === 1183 || code === 1186 || code === 1189 || code === 1192 || code === 1195 || code === 1198 || code === 1201 || code === 1240 || code === 1243 || code === 1246 || code === 1063) return 'rain.svg'
+    if (code === 1210 || code === 1213 || code === 1216 || code === 1219 || code === 1222 || code === 1225 || code === 1255 || code === 1258 || code === 1066 || code === 1114) return 'snow.svg'
+    if (code === 1000) return 'clear.svg'
+    if (code === 1003 || code === 1006) return 'clouds.svg'
+    else return 'atmosphere.svg'
+}
+
+function getCurrentDate() {
+    const currentDate = new Date()
+    const options = {
+        weekday: 'short',
+        day: '2-digit',
+        month: 'short'
+    }
+
+    return currentDate.toLocaleDateString('en-GB', options)
+}
+
 async function updateWeatherInfo(city) {
     const weatherData = await getFetchData('current.json', city)
     if(weatherData.error) {
         showDisplaySection(notFoundSection)
         return
     }
+
+    console.log(weatherData);
+
+    const {
+        location: { name },
+        current: { 
+            temp_c, 
+            humidity, 
+            wind_kph, 
+            condition: { code, text}
+        }
+
+    } = weatherData
+
+    countryTxt.textContent = name
+    tempTxt.textContent = Math.round(temp_c) + ' °C'
+    conditionTxt.textContent = text
+    humidityValueTxt.textContent = humidity + '%'
+    windValueTxt.textContent = wind_kph + ' KM/h'
+
+    currentDateTxt.textContent = getCurrentDate()
+    weatherSummaryImg.src = `assets/weather/${getWeatherIcon(code)}`
 
     showDisplaySection(weatherInfoSection)
 }
