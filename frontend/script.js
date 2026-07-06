c7a38ea5f4c20e4121e6b6e0232a90e00f9143ce
TarekMoustafaElsayed <150616573+TarekMoustafaElsayed@users.noreply.github.com>
2026-07-06 23:48:15 +0300

    The weather app is production ready now

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://127.0.0.1:8000";
+const API_BASE_URL = "https://prodigy-wd-05-zz16.vercel.app";
 
 const cityInput = document.querySelector('.city-input');
 const searchBtn = document.querySelector('.search-btn');
